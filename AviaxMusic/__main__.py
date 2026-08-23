8c380d0c1c3365f56a95975a29e446e2ae0da830
konbkl <konbkl48@gmail.com>
2026-08-23 07:49:39 +0530

    Update __main__.py

--- AviaxMusic/__main__.py
+++ AviaxMusic/__main__.py
@@ -43,15 +43,18 @@ async def init():
         await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AviaxMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
     await Aviax.decorators()
+    
+    # Updated with your custom username
     LOGGER("AviaxMusic").info(
-        "\x41\x76\x69\x61\x78\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x4e\x65\x78\x47\x65\x6e\x42\x6f\x74\x73"
+        "Aviax Music Started Successfully.\n\nDon't forget to visit @BetaBot_hun"
     )
+    
     await idle()
     await app.stop()
     await userbot.stop()
